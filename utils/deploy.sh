2389abb33c125b1f2a1a0756f63b5255fdde44f0
robert <robert@allegro.no>
2016-03-14 16:15:03 +0100

    Update utils file

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh castlehill69.ddns.net 'cd /home/robert/project-icon-harvest/icon-harvest && git pull origin master && composer install'
+ssh castlehill69.ddns.net 'cd /home/robert/www/project-icon-harvest && git pull origin master && composer install'
