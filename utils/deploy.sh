b3855d0a7db6a409fc7824f1157577ec2ac9bb1d
robert <robert@allegro.no>
2016-03-14 16:11:21 +0100

    Added utils

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ssh castlehill69.ddns.net 'cd /home/robert/project-icon-harvest/icon-harvest && git pull && composer install'
+ssh castlehill69.ddns.net 'cd /home/robert/project-icon-harvest/icon-harvest && git pull origin master && composer install'
