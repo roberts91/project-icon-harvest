a3bdf388ffda9ae1db848330b06cf5321863b5db
robert <robert@allegro.no>
2016-03-19 16:41:45 +0100

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('harvesticons')->dailyAt('12:39');
+        $schedule->command('harvesticons');//->daily();
     }
 }
